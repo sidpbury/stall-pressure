0bcb33df47b7aee6b4a28fa2a1047576c54db9c3
Jeffrey Condell <jdcairtrain@gmail.com>
2025-02-03 14:17:59 -0500

    added recording data to a log file

--- monitor-psi.py
+++ monitor-psi.py
@@ -19,23 +19,46 @@ def monitor_psi(path, interval, use_csv):
                 csvwriter = csv.writer(file)
                 csvwriter.writerows(data["io"])
         else:
-            pass
+            with open(path + "/cpu", 'a') as file:
+                file.write(timestamp + ":\n")
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["cpu"][1])) + '\n')
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["cpu"][2])) + '\n\n')
+            with open(path + "/memory", 'a') as file:
+                file.write(timestamp + ":\n")
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["memory"][1])) + '\n')
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["memory"][2])) + '\n\n')
+            with open(path + "/io", 'a') as file:
+                file.write(timestamp + ":\n")
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["io"][1])) + '\n')
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["io"][2])) + '\n\n')
         time.sleep(interval)
 
 def start_monitoring(path='./psi_data', interval=10, use_csv=False):
+    if not os.path.exists(path):
+        os.mkdir(path)
     if use_csv:
-        if not os.path.exists(path):
-            os.mkdir(path)
         if not os.path.exists(path + "/cpu"):
             os.mkdir(path + "/cpu")
         if not os.path.exists(path + "/memory"):
             os.mkdir(path + "/memory")
         if not os.path.exists(path + "/io"):
             os.mkdir(path + "/io")
-    monitor = multiprocessing.Process(target=monitor_psi, args=(path, interval, csv,))
+    else:
+        with open(path + "/cpu", 'a') as file:
+            file.write("name\tavg10\tavg60\tavg300\ttotal\n")
+        with open(path + "/memory", 'a') as file:
+            file.write("name\tavg10\tavg60\tavg300\ttotal\n")
+        with open(path + "/io", 'a') as file:
+            file.write("name\tavg10\tavg60\tavg300\ttotal\n")
+
+    monitor = multiprocessing.Process(target=monitor_psi, args=(path, interval, use_csv,))
     monitor.start()
     return monitor
 
 def stop_monitoring(monitor):
     monitor.terminate()
     monitor.join()
+
+monitor = start_monitoring()
+time.sleep(20)
+stop_monitoring(monitor)
