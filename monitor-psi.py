3cb2a8cfd4b2cbd708893dfd0b7734b6f1c72da5
Jeffrey Condell <jdcairtrain@gmail.com>
2025-02-03 14:25:05 -0500

    cleaned up code

--- monitor-psi.py
+++ monitor-psi.py
@@ -4,33 +4,29 @@ import time
 import csv
 import os
 
+def log_data(path, data, timestamp, resource):
+    with open(path + "/" + resource, 'a') as file:
+                file.write(timestamp + ":\n")
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data[resource][1])) + '\n')
+                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data[resource][2])) + '\n\n')
+
+def log_csv(path, data, timestamp, resource):
+    with open(path + "/" + resource + "/" + timestamp + ".csv", 'a') as file:
+                csvwriter = csv.writer(file)
+                csvwriter.writerows(data[resource])
+
 def monitor_psi(path, interval, use_csv):
     while True:
         data = psi_info.read_psi_info()
         timestamp = time.ctime()
         if use_csv:
-            with open(path + "/cpu/" + timestamp + ".csv", 'a') as file:
-                csvwriter = csv.writer(file)
-                csvwriter.writerows(data["cpu"])
-            with open(path + "/memory/" + timestamp + ".csv", 'a') as file:
-                csvwriter = csv.writer(file)
-                csvwriter.writerows(data["memory"])
-            with open(path + "/io/" + timestamp + ".csv", 'a') as file:
-                csvwriter = csv.writer(file)
-                csvwriter.writerows(data["io"])
+            log_csv(path, data, timestamp, "cpu")
+            log_csv(path, data, timestamp, "memory")
+            log_csv(path, data, timestamp, "io")
         else:
-            with open(path + "/cpu", 'a') as file:
-                file.write(timestamp + ":\n")
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["cpu"][1])) + '\n')
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["cpu"][2])) + '\n\n')
-            with open(path + "/memory", 'a') as file:
-                file.write(timestamp + ":\n")
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["memory"][1])) + '\n')
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["memory"][2])) + '\n\n')
-            with open(path + "/io", 'a') as file:
-                file.write(timestamp + ":\n")
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["io"][1])) + '\n')
-                file.write('\t'.join(map(lambda x: f"{x:.2f}" if type(x) == float else x, data["io"][2])) + '\n\n')
+            log_data(path, data, timestamp, "cpu")
+            log_data(path, data, timestamp, "memory")
+            log_data(path, data, timestamp, "io")
         time.sleep(interval)
 
 def start_monitoring(path='./psi_data', interval=10, use_csv=False):
@@ -58,7 +54,3 @@ def start_monitoring(path='./psi_data', interval=10, use_csv=False):
 def stop_monitoring(monitor):
     monitor.terminate()
     monitor.join()
-
-monitor = start_monitoring()
-time.sleep(20)
-stop_monitoring(monitor)
