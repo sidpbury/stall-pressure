684dfb6e977a8b16d57b5b6ed99a9afe302b3e02
Jeffrey Condell <jdcairtrain@gmail.com>
2025-02-03 13:20:13 -0500

    added recording psi data to csv files

--- monitor-psi.py
+++ monitor-psi.py
@@ -1,14 +1,41 @@
 import read_psi_info as psi_info
 import multiprocessing
+import time
+import csv
+import os
 
-def monitor_psi(invterval, data):
-    pass
+def monitor_psi(path, interval, use_csv):
+    while True:
+        data = psi_info.read_psi_info()
+        timestamp = time.ctime()
+        if use_csv:
+            with open(path + "/cpu/" + timestamp + ".csv", 'a') as file:
+                csvwriter = csv.writer(file)
+                csvwriter.writerows(data["cpu"])
+            with open(path + "/memory/" + timestamp + ".csv", 'a') as file:
+                csvwriter = csv.writer(file)
+                csvwriter.writerows(data["memory"])
+            with open(path + "/io/" + timestamp + ".csv", 'a') as file:
+                csvwriter = csv.writer(file)
+                csvwriter.writerows(data["io"])
+        else:
+            pass
+        time.sleep(interval)
 
-def start_monitoring(interval=10, data=[]):
-    monitor = multiprocessing.Process(target=monitor_psi, args=(interval, data,))
+def start_monitoring(path='./psi_data', interval=10, use_csv=False):
+    if use_csv:
+        if not os.path.exists(path):
+            os.mkdir(path)
+        if not os.path.exists(path + "/cpu"):
+            os.mkdir(path + "/cpu")
+        if not os.path.exists(path + "/memory"):
+            os.mkdir(path + "/memory")
+        if not os.path.exists(path + "/io"):
+            os.mkdir(path + "/io")
+    monitor = multiprocessing.Process(target=monitor_psi, args=(path, interval, csv,))
     monitor.start()
-    return monitor, data
+    return monitor
 
 def stop_monitoring(monitor):
-    monitor.join()
     monitor.terminate()
+    monitor.join()
